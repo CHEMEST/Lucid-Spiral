f9d35064166269af6b9fc77cba5dc390321a5cd8
CHEMEST <110419273+CHEMEST@users.noreply.github.com>
2025-03-11 13:38:47 -0500

    Fixed null issues in Manager system

--- Globals/Utils.cs
+++ Globals/Utils.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 
 namespace LucidSpiral.Globals
 {
+#nullable enable
     partial class Utils : Node
     {
         // This can technically return a null but if it does that most likely
@@ -23,35 +24,37 @@ namespace LucidSpiral.Globals
         /// <typeparam name="T"></typeparam>
         /// <param name="root"> the owner of the tree you are searching (ie. the root)</param>
         /// <returns></returns>
-        public static T FindManager<T>(Node root) where T : class, IManager 
+        public static T? FindManager<T>(Node root) where T : class, IManager 
         {
             ManagerHub hub = root.GetNodeOrNull<ManagerHub>("ManagerHub");
-            if (hub == null) { throw new NullReferenceException("ManagerHub cannot be found for the searched node. Make sure the right root is being passed"); }
+            if (hub == null) { return null; }
             return hub.GetManager<T>();
         }
-        public static MovementPattern GetActiveMovementPattern(Node root)
+        public static MovementPattern? GetActiveMovementPattern(Node root)
         {
-            return FindManager<MovementManager>(root).GetActiveBehavior() as MovementPattern;
+            return FindManager<MovementManager>(root)?.GetActiveBehavior() as MovementPattern;
         }
         public static void SetState(Node root, State state) 
         {
-            FindManager<StateManager>(root).ActiveState = state;
+            StateManager? stateManager = FindManager<StateManager>(root);
+            if (stateManager == null) return;
+            stateManager.ActiveState = state;
         }
-        public static T FindStatus<T>(Node root) where T : class, IStatus
+        public static T? FindStatus<T>(Node root) where T : class, IStatus
         {
-            StatusManager manager = FindManager<StatusManager>(root);
-            T status = manager.GetStatus<T>();
-            if (status == null) {  return EmptyStatus.Instance as T; }
+            StatusManager? manager = FindManager<StatusManager>(root);
+            T? status = manager?.GetStatus<T>();
             return status;
         }
-        public static CollisionSet FindCollisionSet(Node root, CollisionType type)
+        public static CollisionSet? FindCollisionSet(Node root, CollisionType type)
         {
-            return FindManager<CollisionManager>(root).GetCollisionSet(type);
+            return FindManager<CollisionManager>(root)?.GetCollisionSet(type);
         }
         // this function makes me happy
         public static void ProcessCollisions(Node root, CollisionType rootCollisionType, CollisionType targetCollisionType, Action<CollisionSet> onCollision)
         {
-            CollisionSet nodeCollisionSet = FindCollisionSet(root, rootCollisionType);
+            CollisionSet? nodeCollisionSet = FindCollisionSet(root, rootCollisionType);
+            if (nodeCollisionSet == null) return;
             if (nodeCollisionSet.Type == CollisionType.Empty)
             {
                 throw new NotSupportedException($"No valid {rootCollisionType} collision found for the given node.");
